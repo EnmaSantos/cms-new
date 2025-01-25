c68e7a7cc9580504de2d60bfed8a770f1ff1ea8e
EnmaSantos <del20047@byui.edu>
2025-01-25 14:36:27 -0700

    implement feature switching in header and main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cms-root',
   templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: false
 })
 export class AppComponent {
   title = 'cms';
+  
+
+  selectedFeature: string = 'documents';
+
+
+  switchView(feature: string) {
+    this.selectedFeature = feature;
+  }
 }
