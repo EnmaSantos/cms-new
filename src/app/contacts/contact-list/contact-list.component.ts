3d3cbdd258f3f4b3edafd54b6c87df1a24ac61f9
EnmaSantos <del20047@byui.edu>
2025-01-25 09:25:22 -0700

    initialize Angular project with basic structure and components

---
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-list',
+  standalone: false,
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.css'
+})
+export class ContactListComponent {
+  contacts: Contact[] = [
+    new Contact(
+      '1',
+      'R. Kent Jackson',
+      'jacksonk@byui.edu',
+      '208-496-3771',
+      'https://raw.githubusercontent.com/EnmaSantos/cms/a29f5cef72516895d8dc22f5d04fc0c9850a14c7/src/assets/images/barzeer.jpg',  
+      null
+    ),
+    new Contact(
+      '2',
+      'Rex Barzee',
+      'barzeer@byui.edu',
+      '208-496-3768',
+      'https://raw.githubusercontent.com/EnmaSantos/cms/a29f5cef72516895d8dc22f5d04fc0c9850a14c7/src/assets/images/jacksonk.jpg',   
+      null
+    )
+  ];
+}
