fc0dcd6b7391ec5cc387f583900411fc4178bf25
EnmaSantos <del20047@byui.edu>
2025-02-15 21:31:43 -0700

    Add child routes for document detail and edit components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DocumentsComponent } from './documents/documents.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 import { MessageListComponent } from './messages/message-list/message-list.component';
 import { ContactsComponent } from './contacts/contacts.component';
 
 // Define the routes
 const appRoutes: Routes = [
   { path: '', redirectTo: '/documents', pathMatch: 'full' },
-  { path: 'documents', component: DocumentsComponent },
+  {
+    path: 'documents',
+    component: DocumentsComponent,
+    children: [
+      { path: ':id', component: DocumentDetailComponent }, // Child route for document detail
+      { path: ':id/edit', component: DocumentEditComponent } // Child route for document edit
+    ]
+  },
   { path: 'messages', component: MessageListComponent },
   { path: 'contacts', component: ContactsComponent }
 ];
